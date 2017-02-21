9d1c15a78d75400421aaa6224ea6328e910edc87
eremiev <emil.eremiev@abv.bg>
2017-02-21 14:30:20 +0200

    first upload

---
+++ WebApplication6/Models/Author.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication6.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
