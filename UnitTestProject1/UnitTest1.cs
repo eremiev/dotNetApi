edf0e1999f542edfa8f17e296b0232ee55626b42
eremiev <emil.eremiev@abv.bg>
2017-02-22 09:34:43 +0200

    First api test

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication6.Models;
+using System.Collections.Generic;
+using WebApplication6.Controllers;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GetAllProducts_ShouldReturnAllProducts()
+        {
+            var testbooks = GetTestProducts();
+            var controller = new BooksController(testbooks);
+
+            var result = controller.GetBooks() as List<BookDTO>;
+            Assert.AreEqual(testbooks.Count, result.Count);
+        }
+
+        //[TestMethod]
+        //public async Task GetAllProductsAsync_ShouldReturnAllProducts()
+        //{
+        //    var testProducts = GetTestProducts();
+        //    var controller = new SimpleProductController(testProducts);
+
+        //    var result = await controller.GetAllProductsAsync() as List<Product>;
+        //    Assert.AreEqual(testProducts.Count, result.Count);
+        //}
+
+        //[TestMethod]
+        //public void GetProduct_ShouldReturnCorrectProduct()
+        //{
+        //    var testProducts = GetTestProducts();
+        //    var controller = new SimpleProductController(testProducts);
+
+        //    var result = controller.GetProduct(4) as OkNegotiatedContentResult<Product>;
+        //    Assert.IsNotNull(result);
+        //    Assert.AreEqual(testProducts[3].Name, result.Content.Name);
+        //}
+
+        //[TestMethod]
+        //public async Task GetProductAsync_ShouldReturnCorrectProduct()
+        //{
+        //    var testProducts = GetTestProducts();
+        //    var controller = new SimpleProductController(testProducts);
+
+        //    var result = await controller.GetProductAsync(4) as OkNegotiatedContentResult<Product>;
+        //    Assert.IsNotNull(result);
+        //    Assert.AreEqual(testProducts[3].Name, result.Content.Name);
+        //}
+
+        //[TestMethod]
+        //public void GetProduct_ShouldNotFindProduct()
+        //{
+        //    var controller = new SimpleProductController(GetTestProducts());
+
+        //    var result = controller.GetProduct(999);
+        //    Assert.IsInstanceOfType(result, typeof(NotFoundResult));
+        //}
+
+        private List<BookDTO> GetTestProducts()
+        {
+            var testBooks = new List<BookDTO>();
+            testBooks.Add(new BookDTO { Id = 1, Title = "Title1", AuthorName = "AuthorName1" });
+            testBooks.Add(new BookDTO { Id = 2, Title = "Title2", AuthorName = "AuthorName2" });
+            testBooks.Add(new BookDTO { Id = 3, Title = "Title3", AuthorName = "AuthorName3" });
+            testBooks.Add(new BookDTO { Id = 4, Title = "Title4", AuthorName = "AuthorName4" });
+
+            return testBooks;
+        }
+    }
+}
--- WebApplication6/Controllers/BooksController.cs
+++ WebApplication6/Controllers/BooksController.cs
@@ -17,25 +17,46 @@ namespace WebApplication6.Controllers
     {
         private BookServiceContext db = new BookServiceContext();
 
-        // GET: api/Books
-        public IQueryable<BookDTO> GetBooks()
+        List<BookDTO> books = new List<BookDTO>();
+
+        public BooksController() { }
+
+        public BooksController(List<BookDTO> books)
         {
-            var books = from b in db.Books
-                        select new BookDTO()
-                        {
-                            Id = b.Id,
-                            Title = b.Title,
-                            AuthorName = b.Author.Name
-                        };
+            this.books = books;
+        }
+
+        // GET: api/Books
+        public IEnumerable<BookDTO> GetBooks()
+        {
+            //var books = from b in db.Books
+            //            select new BookDTO()
+            //            {
+            //                Id = b.Id,
+            //                Title = b.Title,
+            //                AuthorName = b.Author.Name
+            //            };
 
             return books;
         }
 
         // GET: api/Books/5
-        [ResponseType(typeof(Book))]
+        [ResponseType(typeof(BookDetailDTO))]
         public async Task<IHttpActionResult> GetBook(int id)
         {
-            Book book = await db.Books.FindAsync(id);
+            var book = await db.Books
+                .Include(b => b.Author)
+                .Select(b => new BookDetailDTO()
+                {
+                    Id = b.Id,
+                    Title = b.Title,
+                    Year = b.Year,
+                    Price = b.Price,
+                    AuthorName = b.Author.Name,
+                    Genre = b.Genre
+                })
+                .SingleOrDefaultAsync(b => b.Id == id);
+
             if (book == null)
             {
                 return NotFound();
@@ -90,6 +111,17 @@ namespace WebApplication6.Controllers
 
             db.Books.Add(book);
             await db.SaveChangesAsync();
+
+            // New code:
+            // Load author name
+            db.Entry(book).Reference(x => x.Author).Load();
+
+            var dto = new BookDTO()
+            {
+                Id = book.Id,
+                Title = book.Title,
+                AuthorName = book.Author.Name
+            };
 
             return CreatedAtRoute("DefaultApi", new { id = book.Id }, book);
         }
